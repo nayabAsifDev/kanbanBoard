f902303a9bc1e094ea7211cfea5437765458a5df
Nayab Asif <nayabasifdev@gmail.com>
2024-03-25 11:42:56 -0400

    refactor: enhance the frontend project
    Co-authored-by: eureka320 <elias.h0320@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
-import { Routes, Route,  Navigate } from 'react-router-dom';
-import SignIn from './pages/signin';
-import SignUp from './pages/signup';
-import Forgot from './pages/forgot';
+import React from "react";
+import Tasks from "./Tasks/Container";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path = "/" element ={<Navigate to = "/signup"/>} />
-        <Route path = "/forgot" element = {<Forgot/>} />
-        <Route path = "/signin" element = {<SignIn/>} />
-        <Route path = "/signup" element = {<SignUp />} />
-      </Routes>
+      <div className="py-3 jumbotron bg-white text-center">
+        <div className="container">
+          <h1>Mini-Kanban</h1>
+          <p className="lead text-muted">
+            This is an example web application of using{" "}
+            <strong>useReducer</strong> of React Hooks as an alternative to
+            Redux.
+          </p>
+        </div>
+      </div>
+      <div className="container-fluid py-5 bg-light">
+        <Tasks />
+      </div>
     </div>
   );
 }
