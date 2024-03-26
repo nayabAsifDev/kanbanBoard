a8977b2299392f16a7ec41e5b7d60e273da85986
Nayab Asif <nayabasifdev@gmail.com>
2024-03-25 20:00:12 -0400

    refactor: create list logic

--- backend/graphqls/mutations/list_mutations.py
+++ backend/graphqls/mutations/list_mutations.py
@@ -9,15 +9,14 @@ from datetime import datetime
 class CreateList(graphene.Mutation):
     class Arguments:
         title = graphene.String(required=True)
-        sort = graphene.String(required=True)
 
     list = graphene.Field(ListType)
 
-    def mutate(self, info, title, sort):
+    def mutate(self, info, title):
         dynamodb = get_dynamodb_client(local=True)
         table = dynamodb.Table('Lists')
         id = title.replace(" ", "-").lower() + '-' + str(uuid.uuid4())
         current_datetime = time2graphql()
-        table.put_item(Item={'id': id, 'title': title, 'sort': sort, 'created': current_datetime, 'updated': current_datetime})
-        return CreateList(list=ListModel(id, title, sort, current_datetime, current_datetime))
+        table.put_item(Item={'id': id, 'key': id, 'title': title, 'sort': 'newest', 'created': current_datetime, 'updated': current_datetime})
+        return CreateList(list=ListModel(id, id, title, 'newest', current_datetime, current_datetime))
 
